38bc78c5a6fde71c9ddf38f3df4cd8c707c901f3
Ankush4135 <30420267+Ankush4135@users.noreply.github.com>
2023-07-29 11:02:17 +0000

    Functional etch a sketch

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-let grid = 64;
-let resolution = (250 / grid).toString() + "px";
+let grid = 16;
 
 const drawArea = document.querySelector('#sketchContainer');
-
+const btnResolution = document.querySelector('#resolution');
 
 function createCanvas(){
-  const canv = document.createElement('div');
-  drawArea.appendChild(canv);
+  let resolution = (250 / grid).toString() + "px";
+  const canvas = document.createElement('div');
+  drawArea.appendChild(canvas);
   for (let i = 1; i <= grid; i++) {
     const column = document.createElement('div');
     column.classList.add('column');
-    canv.appendChild(column);
+    canvas.appendChild(column);
     for (let j = 1; j <= grid; j++) {
       const cell = document.createElement('div');
       cell.classList.add('cell');
@@ -19,9 +19,7 @@ function createCanvas(){
       column.appendChild(cell);
       cell.addEventListener(
         'mousemove',() =>{
-          cell.setAttribute('style', 'background-color:black');
-          cell.style.width = resolution;
-          cell.style.height = resolution;
+          cell.style.backgroundColor = 'black'
         }
       )
     }
@@ -29,3 +27,13 @@ function createCanvas(){
 }
 
 createCanvas();
+
+btnResolution.onclick = () =>{
+  grid = prompt("Enter the grid resolution 1 to 100",16)
+  if (grid != null) {
+    btnResolution.textContent = grid + "X" + grid;
+    createCanvas();
+    const removeCanvas = document.querySelector('#sketchContainer > div')
+    drawArea.removeChild(removeCanvas)
+  }
+}
